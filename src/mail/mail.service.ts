68540fc27e65637a389c1eb0a9dd7dd96d56cc72
Ahmet Ramazan Çapoğlu <52932954+cap-oglu@users.noreply.github.com>
2024-06-19 17:33:36 +0300

    change mail service to nodemail

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,19 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import * as sgMail from '@sendgrid/mail';
+import * as nodemailer from 'nodemailer';
+
 
 @Injectable()
 export class MailService {
+  private transporter;
+
   constructor() {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    this.transporter = nodemailer.createTransport({
+      service: 'gmail', // You can change the service name based on your email provider
+      auth: {
+        user: process.env.EMAIL_USER ,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
   }
 
   async sendVerificationEmail(email: string, username: string, token: string) {
-    const msg = {
-      to: email,
-      from: process.env.SENDGRID_FROM_EMAIL, // Your verified sender
-      subject: 'Verify your email',
-      text: `Please verify your email by clicking on the following link: ${process.env.BASE_URL}/user/verify-email/${username}/${token}`,
+    const mailOptions = {
+      from: process.env.EMAIL_USER , // Sender address
+      to: email, // List of recipients
+      subject: 'Verify your email', // Subject line
+      text: `Please verify your email by clicking on the following link: http://localhost:3000/user/verify-email/${username}/${token}`, // Plain text body
     };
-    await sgMail.send(msg);
+
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('Message sent: %s', info.messageId);
+      return info;
+    } catch (error) {
+      console.error('Error sending email:', error);
+      throw new Error('Failed to send email' + error.message);
+    }
   }
 }
